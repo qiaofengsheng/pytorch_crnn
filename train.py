56f184dcc6ff9f0e74ef8075efa56e21b4ce0daa
乔峰昇.336556 <qiaofengsheng@tal-inc.com.cn>
2023-03-27 11:47:29 +0800

    添加cpu训练

--- train.py
+++ train.py
@@ -4,14 +4,20 @@ from torchvision import transforms
 from model.model import *
 from torch import nn,optim
 import os
-os.environ['CUDA_VISIBLE_DEVICES']='0,1,2,3'
+os.environ['CUDA_VISIBLE_DEVICES']='7'
 
+is_use_cuda = True
 
-local_rank,device = use_ddp()
+if is_use_cuda:
+    local_rank,device = use_ddp()
+else:
+    device = torch.device("cpu")
 
 model = CRNN(32,3,28,512).to(device)
-model=torch.nn.parallel.DistributedDataParallel(model,device_ids=[local_rank],output_device=local_rank)
 
+if is_use_cuda:
+    model=torch.nn.parallel.DistributedDataParallel(model,device_ids=[local_rank],output_device=local_rank)
+    
 
 train_transform = transforms.Compose([
     transforms.ToTensor()
@@ -20,19 +26,22 @@ val_transform = transforms.Compose([
     transforms.ToTensor()
 ])
 train_dataset = CrnnDataset(
-    image_path='./data/image',
-    label_path='./data/label.txt',
+    image_path='/data/qfs/study_demo/crnn_pytorch/data/image',
+    label_path='/data/qfs/study_demo/crnn_pytorch/data/label.txt',
     transform=train_transform,
-    dict_path='./data/dicts.txt'
+    dict_path='/data/qfs/study_demo/crnn_pytorch/data/dicts.txt'
 )
 
-train_sampler = DistributedSampler(train_dataset)
-train_loader = DataLoader(dataset=train_dataset,batch_size=64,sampler=train_sampler,collate_fn=train_dataset.collate_fn)
+if is_use_cuda:
+    train_sampler = DistributedSampler(train_dataset)
+    train_loader = DataLoader(dataset=train_dataset,batch_size=64,sampler=train_sampler,collate_fn=train_dataset.collate_fn)
+else:
+    train_loader = DataLoader(dataset=train_dataset,batch_size=64,shuffle=True,collate_fn=train_dataset.collate_fn)
 
 loss_fn = torch.nn.CTCLoss(blank=0)
 optimizer = optim.Adam(model.parameters())
 
-save_model_path = "./weights"
+save_model_path = "/data/qfs/study_demo/crnn_pytorch/weights"
     
 for i in range(5000):
     train_one_epoch(1,model,device,loss_fn,optimizer,train_loader,train_loader,save_model_path)
